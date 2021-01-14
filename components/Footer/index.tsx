6002dcba9558302e9d6ebbe9fa909ad79511b5de
Saveliy <seva007@hotmail.com>
2021-01-14 22:53:05 +0300

    Correct Footer, SCSS

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,7 @@
 
-import Logo from "../Logo";
-
 const Footer: React.FC = () => {
     return <footer>
-        <Logo />
-        <p>Made with love by @ssandry. MIT license  </p>
-        <p>Deploy by Vercel</p>
+
     </footer>
 }
 
