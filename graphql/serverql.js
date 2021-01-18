0cb54844a8f55978e073162e53dd286ed0eec5b3
Saveliy <seva007@hotmail.com>
2021-01-18 18:00:46 +0300

    Correct schema gql

--- graphql/serverql.js
+++ graphql/serverql.js
@@ -1,92 +1,20 @@
-const express = require('express');
-const { buildSchema } = require('graphql');
-const {graphqlHTTP} = require('express-graphql');
-let port = 3008;
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const { readFileSync } = require("fs");
+
+let PORT = 3008;
+
+const SCHEMAGQL = readFileSync(`${__dirname}/schema.gql`, { encoding: "utf8" })
 
 const data = require("../data/data.json")
 
-let schema = buildSchema(`
-
-    type Card {
-        id: ID!
-        h3: String!
-        h6top: String!
-        h6bot: String!
-
-        img: String
-        imgs: [String]
-    }
-
-    type About {
-        img: String
-        paragraphs: [String]
-    }
-
-    type About2 {
-        mostPopularFilm: Card!
-        paragraphs: [String]
-    }
-
-    type Film {
-        id: ID!
-        title: String!
-        producedBy: String!
-        coverIMG: String!
-        genres: [String]
-        countries: [String]
-        year: String!
-        producer: Card!
-        collage: String!
-        briefAbout: String!
-        about: About!
-        acters: [Card]!
-    }
-
-    type Person {
-        id: ID!
-        name: String!
-        title: String!
-        countries: [String]!
-        imgs: [String]!
-        type: String!
-        about: About2!
-        briefAbout: String!
-        filmography: [Card]!
-        yearsPopular: [String]!
-    }
-
-    type timeSections {
-        title: String!
-        p: [String]
-    }
-
-    type Time {
-        id: ID!
-        title: String!
-        sections: [timeSections]!
-        bestMovies: [Card]!
-    }
-
-    type Query {
-        getAllFilms: [Film]!
-        getAllPersons: [Person]!
-
-        getProducers: [Person]!
-        getActers: [Person]!
-
-        getPerson(id: ID!): Person!
-        getFilm(id: ID!): Film!
-
-        getTime(id: ID!): Time!
-        getAllTimes: [Time]!
-    }
-
-`);
+let schema = buildSchema(SCHEMAGQL);
 
 let root = {
 
     getAllFilms: () => {
-      return data.films;
+        return data.films;
     },
     getAllPersons: () => {
         return data.persons;
@@ -123,5 +51,5 @@ app.use("/", graphqlHTTP({
     graphiql: true
 }))
 
-app.listen(port);
-console.log('GraphQL API server running at localhost: ' + port);
+app.listen(PORT);
+console.log('GraphQL API server running at localhost: ' + PORT);
