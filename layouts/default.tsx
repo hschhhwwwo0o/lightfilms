004676903496d2b3974dbc2954d15c960568c2bf
Saveliy <seva007@hotmail.com>
2021-01-31 16:25:50 +0300

    Correct DefaultLayout

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,7 +1,7 @@
 import TheHeader from "../components/TheHeader/index";
 import TheFooter from "../components/TheFooter/index";
 
-const DefaultLayout = ( { children } ) => {
+const DefaultLayout = ({ children }) => {
     return <>
         <TheHeader />
             { children }
