4ce5b61335f38ce997333fa17925d55d60287287
Saveliy <seva007@hotmail.com>
2021-02-09 18:12:53 +0300

    Update __defs

--- public/__defs.py
+++ public/__defs.py
@@ -4,6 +4,9 @@ from PIL import Image
 FILMS_DIR = os.path.abspath('public/data/films')
 FILMS_IMAGES = os.listdir(FILMS_DIR)
 
+PERSONS_DIR = os.path.abspath('public/data/persons')
+PERSONS_IMAGES = os.listdir(PERSONS_DIR)
+
 
 def __remove():
 
@@ -11,6 +14,13 @@ def __remove():
 
         if image.split(".")[1] == "jpg" or image.split(".")[1] == "jpeg" or image.split(".")[1] == "png":
             pathi = os.path.abspath("public/data/films") + '\\' + image
+            os.remove(pathi)
+
+
+    for image in PERSONS_IMAGES:
+
+        if image.split(".")[1] == "jpg" or image.split(".")[1] == "jpeg" or image.split(".")[1] == "png":
+            pathi = os.path.abspath("public/data/persons") + '\\' + image
             os.remove(pathi)
 
 
@@ -25,8 +35,16 @@ def __convert():
 
             im = Image.open( os.path.abspath("public/data/films") + '\\' + image ).convert("RGB")
             name = "public/data/films/" + image.split(".")[0] + ".webp"
-            print(name)
             im.save(name, "webp")
+
+
+    for image in PERSONS_IMAGES:
+
+            if image.split(".")[1] == "jpg" or image.split(".")[1] == "jpeg" or image.split(".")[1] == "png":
+
+                im = Image.open( os.path.abspath("public/data/persons") + '\\' + image ).convert("RGB")
+                name = "public/data/persons/" + image.split(".")[0] + ".webp"
+                im.save(name, "webp")
 
 
     print("Succesful convert")
