3f851a531ace7f2d4e53bf73fa0322269a557d4d
Saveliy <seva007@hotmail.com>
2021-02-23 20:35:08 +0300

    Correct Card

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -14,34 +14,39 @@ interface ICardProps {
    type: "single" | "double"
 }
 
-const Card: React.FC<ICardProps> = (props) => {
-    return <LazyLoad height = { 300 } offset = { 50 }><article>
-        <Link href={props.HREF} as = {props.AS}>
-            <a 
-                className = { 
-                    props.type === "single" 
-                    ?
-                    "post__preview_single" 
-                    :
-                    "post__preview_double"
-                }
-            >
-                <div className = "img" >
-                    <div className="res" style = {{ position: "relative" }} >
-                        <img src={props.img} alt={props.ALT} />
+const Card: React.FC<ICardProps> = ( props ) => {
+    return <LazyLoad height={ 300 } offset={ 50 }>
+        <article>
+            <Link href={ props.HREF } as={ props.AS }>
+                <a 
+                    className={ 
+                        props.type === "single" ?
+                        "post__preview_single" : "post__preview_double"
+                    }
+                >
+                    <div className="img" >
+                        <div className="res" style={{ position: "relative" }} >
+                            <img src={ props.img } alt={ props.ALT } />
+                        </div>
+                        <div className="res" style = {{ position: "relative" }}>
+                            <img 
+                                src={ 
+                                    props.type === "double" ? 
+                                    props.img2 : props.img
+                                } 
+                                alt={ props.ALT } 
+                            />
+                        </div>
                     </div>
-                    <div className="res" style = {{ position: "relative" }}>
-                        <img src={props.type === "double" ? props.img2 : props.img} alt={props.ALT} />
+                    <div className="tags">
+                        <h6 className="top">{ props.h6top }</h6>
+                        <h3>{ props.h3 }</h3>
+                        <h6>{ props.h6bot }</h6>
                     </div>
-                </div>
-                <div className = "tags">
-                    <h6 className = "top">{ props.h6top }</h6>
-                    <h3>{ props.h3 }</h3>
-                    <h6>{ props.h6bot }</h6>
-                </div>
-            </a>
-        </Link>
-    </article></LazyLoad>
+                </a>
+            </Link>
+        </article>
+    </LazyLoad>
 }
 
 export default Card;
