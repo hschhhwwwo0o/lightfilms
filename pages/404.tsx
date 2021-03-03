ccdf9ed2d9cce6d886e7695e3a053ed6f517996b
Saveliy <seva007@hotmail.com>
2021-03-03 20:43:37 +0300

    Correct syntax

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import TheHeader from "../components/TheHeader/index";
 const C404: React.FunctionComponent = () => {
     return <>
         <TheHeader />
-        <div className = "custom404">
+        <div className="custom404">
             <div>
                 <h1>404</h1>
                 <p>Page not found</p>
