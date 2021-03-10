84bf83d4392c91f833c71fd6038e56066b4ead96
Saveliy <seva007@hotmail.com>
2021-03-10 16:59:06 +0300

    Correct imports

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,5 +1,5 @@
-import TheHeader from "../components/TheHeader/index";
-import TheFooter from "../components/TheFooter/index";
+import TheHeader from "../components/TheHeader";
+import TheFooter from "../components/TheFooter";
 
 const DefaultLayout = ({ children }) => {
     return <>
