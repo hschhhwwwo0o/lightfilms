cbeeb4d83987d70c7cf2227fe8b69ed5e54720ab
Saveliy <seva007@hotmail.com>
2021-03-11 07:42:51 +0300

    Correct syntax

--- assets/utils/__filter.ts
+++ assets/utils/__filter.ts
@@ -3,7 +3,6 @@ import { IFilmCard, IPersonCard } from "../../interfaces";
 export function __filterByGenre(crt: string, arr: IFilmCard[]) {
     if( crt === "all" ) return arr
     if( crt !== "all" ) {
-
         if (crt !== "") {
             crt = crt[0].toUpperCase() + crt.slice(1);
         }
@@ -11,7 +10,6 @@ export function __filterByGenre(crt: string, arr: IFilmCard[]) {
         return (arr.filter( (film) => {
             return film.genres[0] === crt || film.genres[1] === crt || film.genres[2] === crt || film.genres[3] === crt
         } ))
-
     }
 }
 
@@ -21,7 +19,6 @@ export function __filterByYear(crt: string, arr: IFilmCard[]) {
         return (arr.filter( (film) => {
             return film.year[2] === crt[2]
         } ))
-
     }
 }
 
@@ -41,7 +38,6 @@ export function __filterByCountry(crt: string, arr: IPersonCard[]) {
         return (arr.filter( (film) => {
             return film.countries[0] === crt || film.countries[1] === crt || film.countries[2] === crt
         } ))
-
     }
 }
 
