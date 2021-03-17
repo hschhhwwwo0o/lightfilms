45a2022e8a6f8e8acd33073c49aa2651e90a305a
Saveliy <seva007@hotmail.com>
2021-03-17 18:25:35 +0300

    Use Meta component

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { IFilmCard } from "../interfaces";
 import { __QLFilms } from "../assets/utils/__ql";
 import { __filterFilms } from "../assets/utils/__filter";
 
-import Head from "next/head";
+import Meta from "../components/Meta";
 import DefaultLayout from "../layouts/default";
 import Chooser from "../components/Chooser";
 import Grid from "../components/Grid";
@@ -24,35 +24,13 @@ const IndexPage: React.FC<IndexPageProps> = ({ films }) => {
     const [year, setYear]   = useState("all")
 
     return <DefaultLayout>
-        <Head>
-            <title>LIGHTFILMS</title>
-
-            {/* Standart meta */}
-            <meta 
-                name="keywords" 
-                content="LIGHTFILMS, Movies in black and white, Retro movies, Classics of cinematograph, cinematography, black and white movies, Full-length cinema, Fiction cinema, History cinema, LIGHT FILMS, Light Films," 
-            />
-            <meta 
-                name="description" 
-                content={`LIGHTFILMS - More about cinema. Read about the great films of the last century. All the best black and white movies. Learning the shooting style of great directors. 1950 cinema,  1960 cinema, 1970 cinema, 1980 cinema`} 
-            />
-            <link rel="canonical" href="https://lightfilms-ssandry.vercel.app/" />
-
-            {/* Open Graph meta */}
-            <meta 
-                property="og:url" 
-                content={ `https://lightfilms-ssandry.vercel.app/` } 
-            />
-            <meta property="og:title" content="LIGHTFILMS - Learn more about great movies & persons" />
-            <meta 
-                property="og:description" 
-                content="LIGHTFILMS is a website for fans of old and high-quality movies. Here you will find the best movies of the past time. There is a collection of more than 30 films from popular to avant-garde."
-            />
-
-            {/* Open Graph Twitter */}
-            <meta name="twitter:title" content="LIGHTFILMS - Learn more about great movies & persons" />
-            <meta name="twitter:description" content="LIGHTFILMS is a website for fans of old and high-quality movies. Here you will find the best movies of the past time. There is a collection of more than 30 films from popular to avant-garde." />
-        </Head>
+        <Meta 
+            titleShort="LIGHTFILMS"
+            titleLong="LIGHTFILMS - Learn more about great movies & persons"
+            description="LIGHTFILMS is a website for fans of old and high-quality movies. Here you will find the best movies of the past time. There is a collection of more than 30 films from popular to avant-garde."
+            url="https://lightfilms-ssandry.vercel.app/"
+            keywords="LIGHTFILMS, Movies in black and white, Retro movies, Classics of cinematograph, cinematography, black and white movies, Full-length cinema, Fiction cinema, History cinema, LIGHT FILMS, Light Films"
+        />
         <Chooser h1="CINEMA">
             <ul id="countrys">
                 <li 
