69376a3e4a8ebc0098ae5ddeafd03fde947772c9
Saveliy <seva007@hotmail.com>
2021-03-21 10:51:04 +0300

    Enable scale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import "../assets/scss/global.scss";
 function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=0.0, minimum-scale=1.0, maximum-scale=1.0" />
     </Head>
     <Component { ...pageProps } />
   </>
