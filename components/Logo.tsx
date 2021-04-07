953ca6f0f4b78800e097b0bbd7fee7e5a06bc9e5
Saveliy <seva007@hotmail.com>
2021-04-07 18:51:09 +0300

    SNTHX

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 const Logo: React.FunctionComponent = () => {
-    return <Link href="/">
-        <a className="logo">
-            LIGHTFILMS
-        </a>
-    </Link>
+    return <>
+        <Link href="/">
+            <a className="logo">
+                LIGHTFILMS
+            </a>
+        </Link>
+    </>
 }
 
 export default Logo;
