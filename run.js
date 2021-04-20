938d2b73d26932dd0a73b3feae52a93411a20ae7
Saveliy <seva007@hotmail.com>
2021-04-20 17:03:42 +0300

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,7 @@
 console.log(" ");
+console.log("Deployment: https://lightfilms-ssandry.vercel.app/");
+console.log("Deployment thanks to Vercel");
+console.log(" ");
 console.log("MODE=development");
 console.log("DEV_SERVER=http://localhost:3000");
 console.log("DEV_GRAPHQL_SERVER=http://localhost:3008");
