218b32db8108d4ff062959b1b4b29a36b5f4acc4
Saveliy <seva007@hotmail.com>
2021-05-07 05:49:35 +0300

    Ref

--- components/TheHeader/index.tsx
+++ components/TheHeader/index.tsx
@@ -1,19 +1,26 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-
 import Logo from "../Logo";
 
-const Header: React.FC = () => {
 
-    const _router = useRouter();
-    const brightColorLink = "#e0e0e0";
+const Header: React.FunctionComponent = () => {
+
+    const __router = useRouter();
+
+    function __isRoute(route: string) {
+        if( __router.pathname === route ) {
+            return "#e0e0e0"
+        } else {
+            return ""
+        }
+    }
 
     return <>
         <header id="header">
             <div className="header__container">
                 <Logo />
                 <input type="checkbox" id="checkbox" />
-                <label htmlFor="checkbox" className="burger-menu">
+                <label htmlFor="checkbox" className="header__burger-menu">
                     <div></div>
                     <div></div>
                     <div></div>
@@ -22,61 +29,35 @@ const Header: React.FC = () => {
                     <ul>
                         <li>
                             <Link href="/time">
-                                <a 
-                                    style={{ 
-                                        color: _router.pathname === "/time" ? 
-                                        brightColorLink : ""
-                                    }}
-                                >
+                                <a style={{ color: __isRoute("/time") }}>
                                     time
                                 </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/">
-                                <a 
-                                    style={{ 
-                                        color: _router.pathname === "/"
-                                        ? 
-                                        brightColorLink : "" 
-                                    }}
-                                >
+                                <a style={{ color: __isRoute("/") }}>
                                     cinema
                                 </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/producers">
-                                <a 
-                                    style={{ 
-                                        color: _router.pathname === "/producers" ? 
-                                        brightColorLink : "" 
-                                    }}
-                                >
+                                <a style={{ color: __isRoute("/producers") }}>
                                     producers
                                 </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/acters">
-                                <a 
-                                    style={{ 
-                                        color: _router.pathname === "/acters" ? 
-                                        brightColorLink : "" 
-                                    }}
-                                >
+                                <a style={{ color: __isRoute("/acters") }}>
                                     acters
                                 </a>
                             </Link>
                         </li>
                         <li>
                             <Link href="/soon">
-                                <a 
-                                    style={{ 
-                                        color: _router.pathname === "/soon" ? 
-                                        brightColorLink : "" 
-                                    }}
-                                >
+                                <a style={{ color: __isRoute("/soon") }}>
                                     genres
                                 </a>
                             </Link>
