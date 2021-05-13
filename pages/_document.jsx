513be83384dc729f7ee872e0d1c10bc8da9864f7
Saveliy <seva007@hotmail.com>
2021-05-13 22:20:44 +0300

    CS

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -21,7 +21,6 @@ export default class __Document extends Document {
                 <Head>
                     <TheMeta />
 
-                    {/* Fonts */}
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link 
                         href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" 
