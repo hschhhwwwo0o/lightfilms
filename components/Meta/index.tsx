d6badddc1f24cbf1dcdc27008b42d618844aef29
Saveliy <seva007@hotmail.com>
2021-05-16 11:34:45 +0300

    CS

--- components/Meta/index.tsx
+++ components/Meta/index.tsx
@@ -3,21 +3,21 @@ import { IMetaInterface } from "./interface";
 
 const Meta: React.FC<IMetaInterface> = ({ titleShort, titleLong, description, url, keywords }) => {
     return <Head>
-        <title>{ titleShort }</title>
+        <title>{titleShort}</title>
 
         {/* Standart meta */}
-        <meta name="description" content={ description } />
-        <meta name="keywords" content={ keywords } />
-        <link rel="canonical" href={ url } />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <link rel="canonical" href={url} />
 
         {/* Open Graph meta */}
-        <meta property="og:title" content={ titleLong } />
-        <meta property="og:url" content={ url } />
-        <meta property="og:description" content={ description } />
+        <meta property="og:title" content={titleLong} />
+        <meta property="og:url" content={url} />
+        <meta property="og:description" content={description} />
 
         {/* Open Graph Twitter */}
-        <meta name="twitter:title" content={ titleLong } />
-        <meta name="twitter:description" content={ description } />
+        <meta name="twitter:title" content={titleLong} />
+        <meta name="twitter:description" content={description} />
     </Head>
 }
 
