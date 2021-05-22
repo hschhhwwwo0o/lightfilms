6d44898c761c6e3a1d3283dcff311f5ad7be2090
Saveliy <seva007@hotmail.com>
2021-05-22 09:10:10 +0300

    CF

--- assets/utils/__filter.ts
+++ assets/utils/__filter.ts
@@ -1,67 +1,71 @@
 import { IFilmCard, IPersonCard } from "../../interfaces";
 
 export function __filterByGenre(crt: string, arr: IFilmCard[]) {
-
-    if( crt === "all" ) return arr
-
-    if( crt !== "all" ) {
-
+    if(crt === "all") return arr;
+    if(crt !== "all") {
         if (crt !== "") {
             crt = crt[0].toUpperCase() + crt.slice(1);
-        }
+        };
 
-        return (arr.filter( (film) => {
-            return film.genres[0] === crt || film.genres[1] === crt || film.genres[2] === crt || film.genres[3] === crt
-        } ))
-    }
-
-}
+        return (
+            arr.filter((film) => {
+                return (
+                    film.genres[0] === crt || 
+                    film.genres[1] === crt || 
+                    film.genres[2] === crt || 
+                    film.genres[3] === crt
+                );
+            })
+        );
+    };
+};
 
 export function __filterByYear(crt: string, arr: IFilmCard[]) {
-
-    if( crt === "all" ) return arr
-
-    if( crt !== "all" ) {
-        return (arr.filter( (film) => {
-            return film.year[2] === crt[2]
-        } ))
-    }
-
-}
+    if(crt === "all") return arr;
+    if(crt !== "all") {
+        return (
+            arr.filter((film) => {
+                return film.year[2] === crt[2];
+            })
+        )
+    };
+};
 
 export function __filterByYearsPersons(crt: string, arr: IPersonCard[]) {
-    
-    if( crt === "all" ) return arr
-
-    if( crt !== "all" ) {
-        return (arr.filter( (person) => {
-            return person.yearsPopular[0][2] === crt[2]
-        } ))
-
-    }
-}
+    if(crt === "all") return arr;
+    if(crt !== "all") {
+        return (
+            arr.filter((person) => {
+                return person.yearsPopular[0][2] === crt[2];
+            })
+        );
+    };
+};
 
 export function __filterByCountry(crt: string, arr: IPersonCard[]) {
-
-    if( crt === "all" ) return arr
-
+    if( crt === "all" ) return arr;
     if( crt !== "all" ) {
-        return (arr.filter( (film) => {
-            return film.countries[0] === crt || film.countries[1] === crt || film.countries[2] === crt
-        } ))
-    }
-    
-}
+        return (
+            arr.filter((film) => {
+                return (
+                    film.countries[0] === crt || 
+                    film.countries[1] === crt || 
+                    film.countries[2] === crt
+                );
+            })
+        );
+    };
+};
 
 export function __filterFilms(crts: [string, string?], arr: IFilmCard[]) {
-    return __filterByGenre( crts[0], __filterByYear(crts[1], arr) )
+    return __filterByGenre(crts[0], __filterByYear(crts[1], arr))
 }
 
 export function __filterPersons(crts: [string, string?], arr: IPersonCard[]) {
-    return __filterByCountry( crts[0], __filterByYearsPersons(crts[1], arr) )
+    return __filterByCountry(crts[0], __filterByYearsPersons(crts[1], arr))
 }
 
 export default {
     __filterFilms, 
     __filterPersons
-}
+};
